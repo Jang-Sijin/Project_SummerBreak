b69872b6567560e09de7865136b40dc42b892229
LimeSharks <80079865+LimeSharks@users.noreply.github.com>
2022-02-28 22:30:11 +0900

    22_02_28_AnimatiorController_Update
    1. 애니메이터 컨트롤러 스크립트 추가
    2. 글라이딩, 플랩 애니메이션 수정

---
+++ Assets/2.IngameScene/Scripts/Player/AnimationController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private PlayerInputManager m_inputManager;
+    private PlayerMovement m_PlayerMovement;
+
+    private Animator m_animation;
+    void Start()
+    {
+        m_animation = GetComponent<Animator>();
+        m_inputManager = GetComponent<PlayerInputManager>();
+        m_PlayerMovement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            m_animation.SetBool("Run", m_PlayerMovement.currentState == PlayerMovement.playerState.runState);
+            
+            m_animation.SetBool("Walk", m_PlayerMovement.currentState == PlayerMovement.playerState.walkState);
+
+            m_animation.SetBool("Jump", m_PlayerMovement.currentState == PlayerMovement.playerState.jumpState);
+            
+            m_animation.SetBool("Ground_idle", m_PlayerMovement.currentState == PlayerMovement.playerState.Ground_idleState);
+            
+            m_animation.SetBool("Swimming", m_PlayerMovement.currentState == PlayerMovement.playerState.swimmingState);
+
+            m_animation.SetBool("Swim_idle", m_PlayerMovement.currentState == PlayerMovement.playerState.Swim_idleState);
+            
+            m_animation.SetBool("Gliding", m_PlayerMovement.currentState == PlayerMovement.playerState.glideState);
+            
+            m_animation.SetBool("Falling", m_PlayerMovement.currentState == PlayerMovement.playerState.failState);
+
+            m_animation.SetBool("Flap", m_PlayerMovement.currentState == PlayerMovement.playerState.flapState);
+            
+            m_animation.SetBool("Climb_idle",m_PlayerMovement.currentState == PlayerMovement.playerState.climb_idleState);
+            
+            m_animation.SetBool("Climbing",m_PlayerMovement.currentState == PlayerMovement.playerState.climbing);
+    }
+}
