afb54ea39b0ca3901cee18dacbdcdcb261044afc
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2022-04-07 20:36:54 +0900

    220407_Merge
    1. 220404_Branch_JangSijin 병합

---
+++ Assets/Manager/SaveSystem/Scripts/SaveInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class SaveInfo
+{
+    public string name;
+    public Vector3 position;
+    public Vector3 rotation;
+    public int hp;
+    public int stamina;
+    public string saveTime;
+
+    public SaveInfo(string name, Vector3 position, Vector3 rotation, int hp, int stamina, string saveTime)
+    {
+        this.name = name;
+        this.position = position;
+        this.rotation = rotation;
+        this.hp = hp;
+        this.stamina = stamina;
+        this.saveTime = saveTime;
+    }
+}
