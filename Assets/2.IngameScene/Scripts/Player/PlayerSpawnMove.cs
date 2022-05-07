19643aa79ec772b13fd351fd10e196b88e9bfd58
LimeSharks <80079865+LimeSharks@users.noreply.github.com>
2022-05-07 13:28:20 +0900

    220507_LandMarkSpawnPoint_Update
    1. 랜드마크 별로 표지판앞으로 순간 이동 가능한 스포너 생성 및 적용

---
+++ Assets/2.IngameScene/Scripts/Player/PlayerSpawnMove.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnMove : MonoBehaviour
+{
+    //키입력
+    [SerializeField]
+    private bool numberPad1KeyDown = false;
+    [SerializeField]
+    private bool numberPad2KeyDown = false;
+    [SerializeField]
+    private bool numberPad3KeyDown = false;
+    [SerializeField]
+    private bool numberPad4KeyDown = false;
+    [SerializeField]
+    private bool numberPad5KeyDown = false;
+
+    private PlayerStatus playerStatus;
+
+    [SerializeField] 
+    private GameObject landMarkSpawnerPoint1;
+    [SerializeField] 
+    private GameObject landMarkSpawnerPoint2;
+    [SerializeField] 
+    private GameObject landMarkSpawnerPoint3;
+    [SerializeField] 
+    private GameObject landMarkSpawnerPoint4;
+    [SerializeField] 
+    private GameObject landMarkSpawnerPoint5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStatus = GameManager.instance.playerGameObject.GetComponent<PlayerStatus>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        
+        if (playerStatus.GetDebugMod())
+        {
+            Interaction();
+        }
+    }
+
+    private void GetInput()
+    {
+        numberPad1KeyDown = Input.GetButtonDown("NumberPad1");
+        
+        numberPad2KeyDown = Input.GetButtonDown("NumberPad2");
+        
+        numberPad3KeyDown = Input.GetButtonDown("NumberPad3");
+        
+        numberPad4KeyDown = Input.GetButtonDown("NumberPad4");
+        
+        numberPad5KeyDown = Input.GetButtonDown("NumberPad5");
+    }
+
+    private void Interaction()
+    {
+        if (numberPad1KeyDown)
+        {
+            this.transform.position = landMarkSpawnerPoint1.transform.position;
+        }
+        else if (numberPad2KeyDown)
+        {
+            this.transform.position = landMarkSpawnerPoint2.transform.position;
+        }
+        else if (numberPad3KeyDown)
+        {
+            this.transform.position = landMarkSpawnerPoint3.transform.position;
+        }
+        else if (numberPad4KeyDown)
+        {
+            this.transform.position = landMarkSpawnerPoint4.transform.position;
+        }
+    }
+}
