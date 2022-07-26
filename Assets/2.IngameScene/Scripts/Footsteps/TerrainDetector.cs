92dd0ccf97987658742ddbcdbd05c2dbf383b4a1
yeonuyeonu <80469013+yeonuyeonu@users.noreply.github.com>
2022-07-27 02:03:27 +0900

    220727_Footsyeps_Update
    1. 발자국 소리 애니메이션 이벤트를 통해 추가
    2. 터레인 레이어 수정
    3. 터레인 그룹 삭제 -> 플레인으로 대체

--- Assets/2.IngameScene/Scripts/Footsteps/TerrainDetector.cs
+++ Assets/2.IngameScene/Scripts/Footsteps/TerrainDetector.cs
@@ -3,17 +3,14 @@
 public class TerrainDetector
 {
     private TerrainData terrainData;
-
     private int alphamapWidth;
     private int alphamapHeight;
     private float[,,] splatmapData;
     private int numTextures;
 
-
-
-    public TerrainDetector(Vector3 playerPos)
+    public TerrainDetector()
     {
-        terrainData = GetClosestCurrentTerrain(playerPos).terrainData;
+        terrainData = Terrain.activeTerrain.terrainData;
         alphamapWidth = terrainData.alphamapWidth;
         alphamapHeight = terrainData.alphamapHeight;
 
@@ -21,13 +18,13 @@ public class TerrainDetector
         numTextures = splatmapData.Length / (alphamapWidth * alphamapHeight);
     }
 
-    private Vector3 ConvertToSplatMapCoordinate(Vector3 playerPos)
+    private Vector3 ConvertToSplatMapCoordinate(Vector3 worldPosition)
     {
         Vector3 splatPosition = new Vector3();
-        Terrain ter = GetClosestCurrentTerrain(playerPos);
+        Terrain ter = Terrain.activeTerrain;
         Vector3 terPosition = ter.transform.position;
-        splatPosition.x = ((playerPos.x - terPosition.x) / ter.terrainData.size.x) * ter.terrainData.alphamapWidth;
-        splatPosition.z = ((playerPos.z - terPosition.z) / ter.terrainData.size.z) * ter.terrainData.alphamapHeight;
+        splatPosition.x = ((worldPosition.x - terPosition.x) / ter.terrainData.size.x) * ter.terrainData.alphamapWidth;
+        splatPosition.z = ((worldPosition.z - terPosition.z) / ter.terrainData.size.z) * ter.terrainData.alphamapHeight;
         return splatPosition;
     }
 
@@ -44,41 +41,9 @@ public class TerrainDetector
                 activeTerrainIndex = i;
                 largestOpacity = splatmapData[(int)terrainCord.z, (int)terrainCord.x, i];
             }
-
         }
+
         return activeTerrainIndex;
     }
-    private Terrain GetClosestCurrentTerrain(Vector3 playerPos)
-    {
-        //Get all terrain
-        Terrain[] terrains = Terrain.activeTerrains;
 
-        //Make sure that terrains length is ok
-        if (terrains.Length == 0)
-            return null;
-
-        //If just one, return that one terrain
-        if (terrains.Length == 1)
-            return terrains[0];
-
-        //Get the closest one to the player
-        float lowDist = (terrains[0].GetPosition() - playerPos).sqrMagnitude;
-        var terrainIndex = 0;
-
-        for (int i = 1; i < terrains.Length; i++)
-        {
-            Terrain terrain = terrains[i];
-            Vector3 terrainPos = terrain.GetPosition();
-
-            //Find the distance and check if it is lower than the last one then store it
-            var dist = (terrainPos - playerPos).sqrMagnitude;
-            if (dist < lowDist)
-            {
-                lowDist = dist;
-                terrainIndex = i;
-            }
-        }
-
-        return terrains[terrainIndex];
-    }
 }
