78c3bcfcb156f503039bf12007101fc0722cc3ea
yeonuyeonu <80469013+yeonuyeonu@users.noreply.github.com>
2022-07-26 22:17:55 +0900

    220726_graphics_upload
    +발자국소리

---
+++ Assets/2.IngameScene/Scripts/Footsteps/FootSteps.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] grassClips;
+    [SerializeField]
+    private AudioClip[] SandClips;
+    [SerializeField]
+    private AudioClip[] StoneClips;
+    [SerializeField]
+    private AudioClip[] DirtClips;
+    [SerializeField]
+    private AudioClip[] SnowClips;
+
+    private AudioSource audioSource;
+    private TerrainDetector terrainDetector0;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        terrainDetector0 = new TerrainDetector(transform.position);
+    }
+    private void Step()
+    {
+        AudioClip clip = GetRandomClip(terrainDetector0);
+        audioSource.PlayOneShot(clip);
+
+    }
+
+    private AudioClip GetRandomClip(TerrainDetector terrainDetector)
+    {
+        int terrainTextureIndex = terrainDetector.GetActiveTerrainTextureIdx(transform.position);
+
+        switch(terrainTextureIndex)
+        {
+            case 0:
+            case 1:
+            case 7:
+            case 9:
+                return grassClips[UnityEngine.Random.Range(0, grassClips.Length)];
+            case 2:
+            case 5:
+                return SandClips[UnityEngine.Random.Range(0, SandClips.Length)];
+            case 3:
+            case 4:
+                return StoneClips[UnityEngine.Random.Range(0, StoneClips.Length)];
+            case 6:
+            case 8:
+            default:
+                return DirtClips[UnityEngine.Random.Range(0, DirtClips.Length)];
+            case 10:
+                return SnowClips[UnityEngine.Random.Range(0, SnowClips.Length)];
+        }
+        
+    }
+}
---
+++ Assets/2.IngameScene/Scripts/Footsteps/TerrainDetector.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+public class TerrainDetector
+{
+    private TerrainData terrainData;
+
+    private int alphamapWidth;
+    private int alphamapHeight;
+    private float[,,] splatmapData;
+    private int numTextures;
+
+
+
+    public TerrainDetector(Vector3 playerPos)
+    {
+        terrainData = GetClosestCurrentTerrain(playerPos).terrainData;
+        alphamapWidth = terrainData.alphamapWidth;
+        alphamapHeight = terrainData.alphamapHeight;
+
+        splatmapData = terrainData.GetAlphamaps(0, 0, alphamapWidth, alphamapHeight);
+        numTextures = splatmapData.Length / (alphamapWidth * alphamapHeight);
+    }
+
+    private Vector3 ConvertToSplatMapCoordinate(Vector3 playerPos)
+    {
+        Vector3 splatPosition = new Vector3();
+        Terrain ter = GetClosestCurrentTerrain(playerPos);
+        Vector3 terPosition = ter.transform.position;
+        splatPosition.x = ((playerPos.x - terPosition.x) / ter.terrainData.size.x) * ter.terrainData.alphamapWidth;
+        splatPosition.z = ((playerPos.z - terPosition.z) / ter.terrainData.size.z) * ter.terrainData.alphamapHeight;
+        return splatPosition;
+    }
+
+    public int GetActiveTerrainTextureIdx(Vector3 position)
+    {
+        Vector3 terrainCord = ConvertToSplatMapCoordinate(position);
+        int activeTerrainIndex = 0;
+        float largestOpacity = 0f;
+
+        for (int i = 0; i < numTextures; i++)
+        {
+            if (largestOpacity < splatmapData[(int)terrainCord.z, (int)terrainCord.x, i])
+            {
+                activeTerrainIndex = i;
+                largestOpacity = splatmapData[(int)terrainCord.z, (int)terrainCord.x, i];
+            }
+
+        }
+        return activeTerrainIndex;
+    }
+    private Terrain GetClosestCurrentTerrain(Vector3 playerPos)
+    {
+        //Get all terrain
+        Terrain[] terrains = Terrain.activeTerrains;
+
+        //Make sure that terrains length is ok
+        if (terrains.Length == 0)
+            return null;
+
+        //If just one, return that one terrain
+        if (terrains.Length == 1)
+            return terrains[0];
+
+        //Get the closest one to the player
+        float lowDist = (terrains[0].GetPosition() - playerPos).sqrMagnitude;
+        var terrainIndex = 0;
+
+        for (int i = 1; i < terrains.Length; i++)
+        {
+            Terrain terrain = terrains[i];
+            Vector3 terrainPos = terrain.GetPosition();
+
+            //Find the distance and check if it is lower than the last one then store it
+            var dist = (terrainPos - playerPos).sqrMagnitude;
+            if (dist < lowDist)
+            {
+                lowDist = dist;
+                terrainIndex = i;
+            }
+        }
+
+        return terrains[terrainIndex];
+    }
+}
